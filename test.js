f9d9c10cf378274aaf5c2887622ba6d8267eec2b
Jason Frame <jason@onehackoranother.com>
2015-08-22 02:18:03 +0100

    parse paths

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 var src = require('fs').readFileSync('test.multi', 'utf8');
 
-console.log(require('./lib/parser').parse(src + "\n"));
+var util = require('util');
+var parsed = require('./lib/parser').parse(src + "\n");
+
+console.log(util.inspect(parsed, {depth: null}));
